bb1d49cdf75a997aae1f1eb8e173e55fabc127d0
unknown <ajayagarwal1600@gmail.com>
2023-02-01 22:19:14 +0530

    nested if

--- practiseconstant.js
+++ practiseconstant.js
@@ -1,7 +1,12 @@
-const x = 9;
-const y = 9;
-if (x === 0 || y === 0) {
-  console.log("x and y =0");
-} else {
-  console.log("x and y is not 0");
+const fruit = "apple";
+if (fruit === "apple") {
+  console.log("this is apple ");
+} else if (fruit === "kiwi") {
+  console.log("this is kiwi");
+} else if (fruit === "banana") {
+  console.log("this is banana");
+} else if (fruit === "grape") {
+  console.log("this is grape");
+}else {
+    console.log("this is not tyoe of fruit");
 }
