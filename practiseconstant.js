bb30fd9babe090bee61be186c58b44d5a1129a41
unknown <ajayagarwal1600@gmail.com>
2023-02-01 22:23:34 +0530

    for loop

--- practiseconstant.js
+++ practiseconstant.js
@@ -1,12 +1,5 @@
-const fruit = "apple";
-if (fruit === "apple") {
-  console.log("this is apple ");
-} else if (fruit === "kiwi") {
-  console.log("this is kiwi");
-} else if (fruit === "banana") {
-  console.log("this is banana");
-} else if (fruit === "grape") {
-  console.log("this is grape");
-}else {
-    console.log("this is not tyoe of fruit");
+let i;
+for(i=0;i<=10;i++)
+{
+    console.log("%d",i);
 }
