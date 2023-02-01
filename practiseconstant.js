e78ce4733fd30d33764d850471878483f7b77be7
unknown <ajayagarwal1600@gmail.com>
2023-02-01 22:56:19 +0530

    switch statements

--- practiseconstant.js
+++ practiseconstant.js
@@ -1,5 +1,18 @@
-let i = 1;
-while (i <= 10) {
-  console.log(i);
-  i += 1;
+let x = 4;
+switch (x) {
+  case 0:
+    console.log("this is for 0");
+    break;
+  case 1:
+    console.log("this is for 1");
+    break;
+  case 2:
+    console.log("this is for 2");
+    break;
+  case 3:
+    console.log("this is for 3");
+    break;
+  case 4:
+    console.log("this is for 4");
+    break;
 }
